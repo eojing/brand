e364c3a4f0e4f366f8d90746fc275ed163c71fd7
eojing <105007348+eojing@users.noreply.github.com>
2024-03-21 13:18:42 +0900

    Add files via upload

--- teaser.js
+++ teaser.js
@@ -4,3 +4,16 @@ const cont = document.querySelectorAll('.cont');
 down_arrow.addEventListener('click',function(){
     window.scrollTo({top : cont[1].offsetTop , behavior: 'smooth'});
 })
+
+
+
+const shareBtn = document.querySelector('.share');
+
+shareBtn.addEventListener('click',function(){
+    window.navigator.share({
+        title: '보물썸', // 공유될 제목
+        text: '보물썸이벤트', // 공유될 설명
+        url: '', // 공유될 URL
+      });
+
+});
