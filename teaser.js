ef743f0a5362691d8878919e12c8e7d8d8c8b4b2
eojing <105007348+eojing@users.noreply.github.com>
2024-03-21 17:09:52 +0900

    Add files via upload

--- teaser.js
+++ teaser.js
@@ -1,12 +1,14 @@
-const down_arrow = document.querySelector('.arrow_click');
+// 아래화살표 클릭시 스크롤 다운
+const down_arrow = document.querySelector('.down_arrow');
 const cont = document.querySelectorAll('.cont');
 
-down_arrow.addEventListener('click',function(){
+down_arrow.addEventListener('click',function(e){
+    e.preventDefault();
     window.scrollTo({top : cont[1].offsetTop , behavior: 'smooth'});
 })
 
 
-
+// 공유하기
 const shareBtn = document.querySelectorAll('.share');
 
 for(let i = 0; i < shareBtn.length; i++){
@@ -17,7 +19,25 @@ for(let i = 0; i < shareBtn.length; i++){
             text: '보물썸이벤트', // 공유될 설명
             url: '', // 공유될 URL
           });
-    
     });
-
 }
+
+
+// 탑버튼
+const topScrollBtn = document.querySelector('.top_scroll_btn');
+
+
+window.addEventListener('scroll',function(){
+    if(this.scrollY >= cont[1].offsetTop){
+        topScrollBtn.classList.add('top_scroll_btn_on');
+    }else{
+        topScrollBtn.classList.remove('top_scroll_btn_on');
+    }
+
+});
+
+topScrollBtn.addEventListener('click',function(e){
+    e.preventDefault()
+    window.scrollTo({top : 0, behavior:'smooth'});
+
+});
